ff31bb0507d67aaeb80245e81e95aa9d7da0b97a
Papawin.s <papawin085087@gmail.com>
2023-06-13 17:47:30 +0700

    test command

---
+++ django/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+django command wait for database start
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
